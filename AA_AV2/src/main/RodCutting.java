891119aaf980a84812485473a06f9a4b9ec17456
Edla Gomes <edla.loann@gmail.com>
2017-05-28 12:58:16 -0300

    Update_01_02

--- AA_AV2/src/main/RodCutting.java
+++ AA_AV2/src/main/RodCutting.java
@@ -1,22 +1,24 @@
 package main;
 
-//Funï¿½ï¿½o que executara o Corte de Hastes
+//Função que executara o Corte de Hastes
 public class RodCutting {
+    static int cutRod(int preço[], int n) {
         if (n <= 0)
             return 0;
         int max_val = Integer.MIN_VALUE;
         for (int i = 0; i<n; i++)
-            max_val = Math.max(max_val, preï¿½o[i] + cutRod(preï¿½o, n-i-1));
+            max_val = Math.max(max_val, preço[i] + cutRod(preço, n-i-1));
         	return max_val;
     }
+    
     /*Programa que ira ler os valores, calcular a quantidade e retornar
-    a melhor soluï¿½ï¿½o*/
+    a melhor solução*/
     public static void main(String args[]) {
     	int arr[] = new int[] {2, 4, 8, 5, 6, 7, 12, 14, 15, 18};
     	int tam = arr.length;
     	
-    	/*No Println vai ser chamada a funï¿½ï¿½o para calcular o corte utilizando
-    	das variï¿½veis arr e tam.*/
-    	System.out.println("Valor Maximo Obtido ï¿½ "+ cutRod(arr, tam));
+    	/*No Println vai ser chamada a função para calcular o corte utilizando
+    	das variáveis arr e tam.*/
+    	System.out.println("Valor Maximo Obtido é "+ cutRod(arr, tam));
 	}
 }
